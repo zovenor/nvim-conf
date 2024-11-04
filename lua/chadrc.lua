8cda080901298109cc5cc9ec959128d9ae6dbfdd
Alexandr <romanenyasasha@gmail.com>
2024-11-05 01:07:45 +0300

    Upload files

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,16 +1,50 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "onedark",
+  theme = "tokyodark",
+  transparency = true,
+  nvdash = {
+    load_on_startup = false,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+    header = {
+      "           ▄ ▄                   ",
+      "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+      "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+      "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+      "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+      "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+      "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+      "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+      "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+  telescope = { style = "borderless" }, -- borderless / bordered
+
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
+    order = nil,
+    modules = nil,
+  },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 return M
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,23 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jj", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<TAB>", ":bnext<CR>")
+map("n", "<S-TAB>", ":bprev<CR>")
+map("n", "<Esc>", ":nohlsearch<CR>")
+map("n", "<leader>d", "<CMD>bd<CR>")
+map("n", "<leader>p", "o<Esc>p")
+map("n", "<S-u>", "<C-r>")
+map("n", "<leader>e", ":NvimTreeToggle<CR>")
+map("n", "<leader><leader>", ":Telescope live_grep<CR>")
+map("n", "<leader>E", ":Telescope diagnostics<CR>")
+map("n", "|", ":vsplit<CR>")
+map("n", "-", ":split<CR>")
+map("n", "<leader>m", ":MarkdownPreview<CR>")
+map("n", "<leader>M", ":MarkdownPreviewStop<CR>")
+
+vim.keymap.set("i", "ff", function()
+  return vim.fn["codeium#Accept"]()
+end, { expr = true, silent = true })
