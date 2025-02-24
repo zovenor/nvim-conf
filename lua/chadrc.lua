bd39229378646aac1eafd75e0236ae7adb5ff7e9
Alexandr <romanenyasasha@gmail.com>
2025-02-24 23:47:44 +0300

    Add transparent

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,13 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "flexoki",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  theme = "flexoki",
+  transparency = true,
 }
 
 -- M.nvdash = { load_on_startup = true }
